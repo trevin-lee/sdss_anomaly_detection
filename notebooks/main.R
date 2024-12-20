a8408ce8f9f0724f4664be4dfb63c913b71ad8c2
CRXKXWYI49 <CRXKXWYI49@protonmail.com>
2024-12-20 15:33:42 -0800

    Initialize Repository

---
+++ notebooks/main.R
@@ -0,0 +1,18 @@
+
+numbers <-c(1,2,3,4,5)
+mean(numbers)
+
+print("hello world")
+
+class(numbers)
+
+sum(numbers)
+
+print(sum(numbers))
+
+mat <- matrix(1:9, nrow=3, ncol=3)
+print(mat)
+
+df <- data.frame(a=1:3, b=4:6, c=7:9)
+
+print(df)
